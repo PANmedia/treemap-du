a9eb476e57c30c2f7d9f51ff6344e5c342a50dfe
David Neilsen <david@panmedia.co.nz>
2013-07-11 14:12:58 +1200

    Update run.php

--- run.php
+++ run.php
@@ -6,15 +6,23 @@ if (!is_dir($path)) {
 if (!is_dir($path)) {
     die('Could not create logs directory: ' . $path);
 }
-exec('du', $du);
-$du = implode(PHP_EOL, $du);
+$cwd = '.';
+if (isset($_SERVER['argv'][1])) {
+    $cwd = $_SERVER['argv'][1];
+}
+$cmd = 'du ' . $cwd;
+echo 'Running ' . $cmd . PHP_EOL;
+exec($cmd, $output);
+$output = implode(PHP_EOL, $output);
 $file = $path . '/du_' . date('Y-m-d_H-i-s') . '.txt';
-file_put_contents($file, $du);
-echo 'Wrote ' . $file . PHP_EOL;
+file_put_contents($file, $output);
+echo 'Wrote ' . $file . PHP_EOL . PHP_EOL;
 
+$cmd = 'df';
+echo 'Running ' . $cmd . PHP_EOL;
+exec($cmd, $output);
 $path = __DIR__ . '/public';
-exec('df', $df);
-$df = implode(PHP_EOL, $df);
+$output = implode(PHP_EOL, $output);
 $file = $path . '/df.txt';
-file_put_contents($file, $df);
-echo 'Wrote ' . $file . PHP_EOL;
+file_put_contents($file, $output);
+echo 'Wrote ' . $file . PHP_EOL . PHP_EOL;
